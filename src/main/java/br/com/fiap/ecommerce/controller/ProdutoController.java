7f2a8e10be70f39167b5066528eca3ef47256fec
Eduardo Santos <eduardo14santos9@gmail.com>
2024-09-20 20:46:22 -0300

    Implementando ModelMapper

--- src/main/java/br/com/fiap/ecommerce/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/ecommerce/controller/ProdutoController.java
@@ -2,13 +2,15 @@ package br.com.fiap.ecommerce.controller;
 
 import java.util.List;
 
-import org.apache.el.stream.Optional;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +19,6 @@ import br.com.fiap.ecommerce.dtos.ProdutoRequestUpdateDto;
 import br.com.fiap.ecommerce.dtos.ProdutoResponseDto;
 import br.com.fiap.ecommerce.model.Produto;
 import br.com.fiap.ecommerce.service.ProdutoService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/produtos")
@@ -25,59 +26,67 @@ public class ProdutoController {
 
     @Autowired
     private ProdutoService produtoService;
-    
+
     @GetMapping
-    public List<ProdutoResponseDto> list(){
-        
-        List<Produto> produtos = produtoService.list();
-        return null;
+    public List<ProdutoResponseDto> list() {
+
+        List<ProdutoResponseDto> dtos = produtoService.list()
+                .stream()
+                // e = igual elemento que chama uma função e por um método o transforma em algo
+                // novo(?)
+                .map(e -> new ProdutoResponseDto().toDto(e))
+                .toList();
+        return dtos;
     }
-    
+
     @PostMapping
-    public ProdutoResponseDto create(@RequestBody ProdutoRequestCreateDto dto){
+    public ProdutoResponseDto create(@RequestBody ProdutoRequestCreateDto dto) {
         // ProdutoRequestCreateDto - > Produto
+        // Produto produto = dto.toModel();
         // Produto saved = produtoService.save();
-        //  Produto - > ProdutoRequestCreateDto
+        // Produto - > ProdutoRequestCreateDto
 
         return null;
     }
 
-    //localhost:8080/produtos/5
+    // localhost:8080/produtos/5
     /*
      * body:{
-     *      "id":5,
-     *       "nome":"Um novo nome"
+     * "id":5,
+     * "nome":"Um novo nome"
      * }
      * 
      */
     @PutMapping("{id}")
-    public ProdutoResponseDto update(@PathVariable Long id, @RequestBody ProdutoRequestUpdateDto dto){
-        
-        if(!produtoService.existsById(id)){
+    public ProdutoResponseDto update(@PathVariable Long id, @RequestBody ProdutoRequestUpdateDto dto) {
+
+        if (!produtoService.existsById(id)) {
             new RuntimeException("Id inexistente");
         }
         return null;
     }
 
     @DeleteMapping("{id}")
-    public void delete(@PathVariable Long id){
-
-        if(!produtoService.existsById(id)){
+    public void delete(@PathVariable Long id) {
+        if (!produtoService.existsById(id)) {
             new RuntimeException("Id inexistente");
         }
+
         produtoService.delete(id);
-        
     }
 
     @GetMapping("{id}")
-    public ProdutoResponseDto findById(@PathVariable Long id){
-        Optiona<Produto> produto = null;
-        if(opt.isPresent()) {
-            Produto produto = opt.get();
-            return null;
-        } else {
-            new  
-        }
-    }
+    public ProdutoResponseDto findById(@PathVariable Long id) {
+        Optional<Produto> opt = produtoService.findById(id);
 
+        Produto produto = null;
+        if (opt.isPresent()) {
+            produto = opt.get();
+
+        } else {
+            new RuntimeException("Id inexistente");
+        }
+
+        return null;
+    }
 }
